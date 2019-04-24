c3ac79f609533518c8048fc74a59c818d61db20b
ningliu <ningliu@keyayun.com>
2019-04-24 17:14:48 +0800

    update uninstall.sh

--- tool/package/uninstall.sh
+++ tool/package/uninstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+set -e
 echo "正在卸载: 冠状动脉生理功能评估软件"
 /usr/bin/pkexec dpkg -r deepvesselffr
+if [$ -eq 126]
+then 
+echo 'you click cancle'
+exit 0
+fi
 rm ~/Desktop/冠状动脉生理功能评估软件.desktop
