242b608f51db5384e8c53dd00882d91b54a6f10a
ningliu <ningliu@keyayun.com>
2019-05-05 19:17:35 +0800

    修改提权方案

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, globalShortcut, dialog } = require('electron')
 const path = require('path'); 
-const addon = require('@cc/findhasp');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -18,9 +17,29 @@ function createWindow () {
   } else {  
     mainWindow.loadURL(`file://${path.join(app.getAppPath(), 'build/index.html')}`);
   }
+  
+  const child = require('child_process')
+.spawn('pkexec', ['dvhasp']);
 
+child.stdout.on('data', function (data) {
+    if(data.indexOf("nohasp") !== -1){
+      dialog.showMessageBox({
+        type: 'info',
+        message: '加密锁检测',
+        detail: '未检测到加密锁,请插入加密锁!',
+        buttons: ['好的'] 
+      });
+    }
+    console.log('stdout: ' + data);
+});
+
+child.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+});
+
+child.stdin.write('1\n'); // Attention!
   // Open the DevTools.
-   mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -28,6 +47,7 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
+    child.stdin.end(); // 结束加密锁检查
   })
 }
 
@@ -52,21 +72,6 @@ if (!gotTheLock) {
   // Create myWindow, load the rest of the app, etc...
   app.on('ready', () => {
     createWindow();
-    setInterval(() => {
-      const hasp = addon.findHasp();
-      if (hasp) {
-
-      } else {
-        dialog.showMessageBox({
-          type: 'info',
-          message: '查找加密锁',
-          detail: 'no',
-          buttons: ['好的']
-        });
-        app.quit();
-      }
-     
-  }, 3000);
   })
 }
 
